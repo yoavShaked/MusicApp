fc90363cf52c19ce6083dfd072ac6a46d1d1b483
yoav shaked <yoavcad@gmail.com>
2018-11-01 20:18:59 +0200

    adding production

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const env = require('dotenv');
+
 env.config();
+require('express-async-errors');
 
 require('./startup/db')(process.env.MONGO_DB_CONNECTION);
 require('./startup/routes')(app);
+require('./startup/production')(app);
 app.listen(process.env.PORT, () => console.log('listen to port: ', process.env.PORT));
--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
 const songs = require('../routes/songs');
+const error = require('../midleware/error');
 
 module.exports = function(app){
     app.use(express.json());
     app.use('/api/users', users);
     app.use('/api/songs', songs);
     app.use('/api/auth', auth);
+    app.use(error);
 }
