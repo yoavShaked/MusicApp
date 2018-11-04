ea77166c6b43d1af0e26867e98673a723675b9e1
yoav shaked <yoavcad@gmail.com>
2018-11-04 13:11:47 +0200

    config cors

--- startup/routes.js
+++ startup/routes.js
@@ -3,9 +3,11 @@ const users = require('../routes/users');
 const auth = require('../routes/auth');
 const songs = require('../routes/songs');
 const error = require('../midleware/error');
+const cors = require('cors');
 
 module.exports = function(app){
     app.use(express.json());
+    app.use(cors());
     app.use('/api/users', users);
     app.use('/api/songs', songs);
     app.use('/api/auth', auth);
